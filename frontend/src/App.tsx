0302a7404d425a14f70dce8c1f7abd6688226007
lifeprofessor <lifeprof@naver.com>
2025-05-27 21:15:44 +0900

    서대전여고 로고 고화질로 변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -182,10 +182,10 @@ function App() {
           alignItems="flex-start"
           justifyContent="flex-start"
         >
-          <img src="/sdj-logo.png" alt="학교 로고" style={{ width: 196, marginRight: 36 }} />
-          <br/>
+          <img src="/sdj-logo2.png" alt="학교 로고" style={{ width: 400, marginRight: 36 }} />
+          <br/><br/>
           <Heading size="md" color="blue.700" mb={8} fontWeight="bold" letterSpacing="tight">
-            활동 영역 선택
+            ☑️ 활동 영역 & 학업 수준 선택
           </Heading>
           <Text fontSize="sm" color="gray.600" mb={2}>
             검토할 활동 영역을 선택하세요
